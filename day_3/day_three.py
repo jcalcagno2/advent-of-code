f82e100ef7159a59452d2f6bee445301654e1156
Joseph Calcagno <j_calca2@uncg.edu>
2024-12-23 13:17:55 -0500

    working on day 3

--- day_3/day_three.py
+++ day_3/day_three.py
@@ -2,22 +2,25 @@ import re
 import sys
 
 
-with open(sys.argv[1], "r") as f:
-    data = f.read()
+def read_input():
+    '''read input file'''
+
+    input_file = open("../day_2/input2.txt","r")
+    return input_file
 
 # pairs = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', data)
 # part1 = sum(prod(map(int, val)) for val in pairs)
-
+data = read_input()
 part1 = 0
 part2 = 0
 enabled = True
 for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data):
-    match inst:
-        case "do()":
+    
+        if inst == "do()":
             enabled = True
-        case "don't()":
+        elif inst == "don't()":
             enabled = False
-        case _:
+        else:
             x, y = map(int, inst[4:-1].split(","))
             part1 += x * y
             if enabled:
